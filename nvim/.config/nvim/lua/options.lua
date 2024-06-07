e5bf1f621967b47f56f7f3559bf859d95317aea1
Sntz91 <tobias.ziegler.8@gmail.com>
2024-06-07 21:08:01 +0200

    added zathura stuff

--- nvim/.config/nvim/lua/options.lua
+++ nvim/.config/nvim/lua/options.lua
@@ -21,4 +21,3 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 		vim.highlight.on_yank()
 	end,
 })
-
--- nvim/.config/nvim/lua/plugins/catppuccin.lua
+++ nvim/.config/nvim/lua/plugins/catppuccin.lua
@@ -9,5 +9,7 @@ return {
 	lazy = false,
 	config = function()
 		vim.cmd.colorscheme("catppuccin")
+		vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+		vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 	end
 }
