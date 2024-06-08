e4c6c560ac07732af3606253e72c1da9a2f90ec4
Sntz91 <tobias.ziegler.8@gmail.com>
2024-06-08 15:04:56 +0200

    changed tmux and zathura copy paste

--- nvim/.config/nvim/lua/plugins/catppuccin.lua
+++ nvim/.config/nvim/lua/plugins/catppuccin.lua
@@ -4,12 +4,12 @@ return {
 	priority = 1000,
 	opts = {
 		term_colors = true,
-		transparent_background = true,
+		--transparent_background = true,
 	},
 	lazy = false,
 	config = function()
 		vim.cmd.colorscheme("catppuccin")
-		vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-		vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+		--vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+		--vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 	end
 }
